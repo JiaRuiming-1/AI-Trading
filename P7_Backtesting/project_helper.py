d824995e566d7af904d099aa22671811995b4aac
JiaRuiming-1 <115370574+JiaRuiming-1@users.noreply.github.com>
2023-05-05 23:05:11 +0800

    Add files via upload

--- P7_Backtesting/project_helper.py
+++ P7_Backtesting/project_helper.py
@@ -8,11 +8,62 @@ from scipy.stats import spearmanr
 from IPython.display import Image
 from sklearn.tree import export_graphviz
 
+import cvxpy as cvx
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.base import clone
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import Bunch
+
+
+class OptimalHoldings():
+    def __init__(self, aversion=.2, factor_max=50.0, factor_min=-50.0, weights_max=0.25, weights_min=0, lambda_reg=.2):
+        self.lambda_reg = lambda_reg
+        self.risk_cap = aversion
+        self.factor_max = factor_max
+        self.factor_min = factor_min
+        self.weights_max = weights_max
+        self.weights_min = weights_min
+
+    def _get_obj(self, weights, alpha_vector, pre_weights, Lambda):
+        assert (len(alpha_vector.columns) == 1)
+        cost_obj = sum(np.dot((weights - pre_weights) ** 2, Lambda))
+        # return cvx.Minimize(-alpha_vector.values.flatten() * weights)
+        return cvx.Minimize(-alpha_vector.values.T * weights + self.lambda_reg * cvx.norm(weights) + cost_obj)
+
+    def _get_constraints(self, weights, factor_betas, risk):
+        assert (len(factor_betas.shape) == 2)
+
+        f = factor_betas.T * weights
+        constraint = [risk <= self.risk_cap,
+                      f <= self.factor_max, f >= self.factor_min,
+                      sum(cvx.abs(weights.T)) <= 1.0,
+                      weights <= self.weights_max, weights >= self.weights_min
+                      ]
+
+        return constraint
+
+    def _get_risk(self, weights, factor_betas, alpha_vector_index, factor_cov_matrix, idiosyncratic_var_vector):
+        f = factor_betas.loc[alpha_vector_index].values.T * weights
+        X = factor_cov_matrix
+        S = np.diag(idiosyncratic_var_vector.loc[alpha_vector_index].values.flatten())
+        return cvx.quad_form(f, X) + cvx.quad_form(weights, S)
+
+    def find(self, alpha_vector, factor_betas, factor_cov_matrix, idiosyncratic_var_vector, pre_weights, Lambda = 2e-6):
+        weights = cvx.Variable(len(alpha_vector))
+
+        risk = self._get_risk(weights, factor_betas, alpha_vector.index, factor_cov_matrix, idiosyncratic_var_vector)
+
+        obj = self._get_obj(weights, alpha_vector, pre_weights, Lambda)
+        constraints = self._get_constraints(weights, factor_betas.loc[alpha_vector.index].values, risk)
+
+        prob = cvx.Problem(obj, constraints)
+        prob.solve(max_iters=50)
+        # prob.solve()
+
+        optimal_weights = np.asarray(weights.value).flatten()
+        return pd.DataFrame(data=optimal_weights, index=alpha_vector.index)
 
 
 class NoOverlapVoter(VotingClassifier):
