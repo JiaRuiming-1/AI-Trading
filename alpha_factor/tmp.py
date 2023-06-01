415bc560820d8eee53df7c0b1550cb743bd2d369
JiaRuiming-1 <115370574+JiaRuiming-1@users.noreply.github.com>
2023-06-01 16:57:36 +0800

    Update tmp.py

--- alpha_factor/tmp.py
+++ alpha_factor/tmp.py
@@ -1,55 +1,78 @@
-def alpha_t1(df):
-    def cal_(data):
-        data['alpha_t1'] = (data['close'].rolling(4).mean() - data['close'].rolling(20).mean())/data['close'].rolling(20).std()
-        data['alpha_t1'] = -data['alpha_t1'] + data['rsi_6'] * 0.2
-        return data
-    
-    df = my_groupby(df, 'ts_code', cal_)
-    return df
+negative_field = ['alpha_021','alpha_022', 'alpha_029', 'alpha_044', 'alpha_078',  'alpha_089', 
+                  'alpha_110', 'alpha_112', 'alpha_128', 'alpha_116', 'alpha_122', 'alpha_131', 'alpha_133', 'alpha_176', ]
+universe[negative_field] = -universe[negative_field]
 
-universe = alpha_t1(universe) 
-
-def alpha_t2(df):
-    def cal_(data):
-        data['alpha_t2'] = -data['log-ret']/ (data['close'].rolling(6).max() - data['close'].rolling(6).min())
-        return data
-    
-    df = my_groupby(df, 'ts_code', cal_)
-    return df
-    
-universe = alpha_t2(universe)     
-
-def alpha_t3(df):
-    def cal_(data):
-        data['alpha_t3'] = -data['log-ret'].rolling(4).sum()/(data['high'] - data['low']).rolling(12).sum()
-        return data
-    
-    df = my_groupby(df, 'ts_code', cal_)
-    return df
-
-universe = alpha_t3(universe)
-
-import random
-universe = universe.sort_values(by=['date','ts_code'])
-all_factors = universe.copy(deep=True)
-start_time = '2023-01-01 00:00:00'
-all_factors = all_factors.loc[all_factors.index>=start_time]
-def return_handle(df):
-    df['returns_2'] = df['log-ret'].shift(-1)
-    noise = abs(random.gauss(0, 0.02))
-    if noise > 0.02:
-        noise=0.005
-    df['returns_2'] = df['returns_2'] + noise*df['vwap'].pct_change().shift(-1).fillna(0.)
-    return df
-all_factors = all_factors.groupby('ts_code').apply(return_handle)
-all_factors = all_factors.replace([np.inf, -np.inf], np.nan).fillna(0.).sort_values(by=['date', 'ts_code'])
-print(universe.shape, all_factors.shape)
-
-
-#'alpha_wt','alpha_rsi', 'alpha_srsi','alpha_t1', 'alpha_t3', 'alpha_029', 'alpha_078',  'alpha_112', 'alpha_131', 'alpha_176',
-display_field  = [
-   'alpha_wt','alpha_cci', 'alpha_rsi', 'alpha_srsi',  'alpha_078', 'alpha_111', 'alpha_131',
-   'alpha_t1', 'alpha_t2',  'alpha_t3', 'alpha_019', 'alpha_t3_fix',
-   #'alpha_006', 'alpha_007', 'alpha_008', 'alpha_012', 'alpha_018', 'alpha_029', 'alpha_039', 
-   #'alpha_061', 'alpha_112', 'alpha_101', 'alpha_176', 'alpha_t2_fix'
+factor_names = [
+   'alpha_atr','alpha_wt', 'alpha_wtr', 'alpha_cci', 'alpha_rsi', 'alpha_t1', 'alpha_t3', 'alpha_t5',
+   'alpha_019', 'alpha_008', 'alpha_078',  'alpha_018', 'alpha_021', 'alpha_022', 
+   'alpha_111', 'alpha_110',   'alpha_131', 'alpha_112', 'alpha_116', 'alpha_128', 'alpha_133',   
+   'alpha_035', 'alpha_061','alpha_089', 'alpha_006','alpha_033',  'alpha_039', 'alpha_040', 'alpha_083', 
 ]
+
+def rescale_rank(data, zscore_features=factor_names):
+    data[zscore_features] = data[zscore_features].rank(method='min',pct=True)
+    data[zscore_features] = (data[zscore_features] - 0.5) * 2
+    return data
+
+bak = universe.copy(deep=True)
+start_time = '2022-11-01 00:00:00'
+universe = universe.loc[universe.index>=start_time]
+universe = my_groupby(universe, 'trade_date', rescale_)
+
+###################################################################################################
+q_num = 5
+alpha_field  = [
+   'alpha_atr','alpha_wt', 'alpha_wtr', 'alpha_cci', 'alpha_rsi', 'alpha_t1', 'alpha_t3', 'alpha_t5',
+   'alpha_019', 'alpha_008', 'alpha_078',  'alpha_018', 'alpha_021', 'alpha_022', 
+   'alpha_111', 'alpha_110',   'alpha_131', 'alpha_112', 'alpha_116', 'alpha_128', 'alpha_133',   
+   'alpha_035', 'alpha_061','alpha_089', 'alpha_033', 'alpha_006', 'alpha_039', 'alpha_040', 'alpha_083', 
+]
+
+df = pd.DataFrame(index=alpha_df.index.unique())
+for dt in tqdm(alpha_df.index.unique()):
+    for feature in alpha_field:
+        tmp = alpha_df.loc[alpha_df.index == dt]
+        tmp['returns_2'] = wins(tmp['returns_2'], -0.03, 0.03) * (1 - 5e-4)
+        df.at[dt, feature] = (tmp['returns_2'] * tmp[feature]).sum()/(tmp[feature].abs().sum()) * 5
+        # calculate quartile 3 returns
+        try:
+            tmp[feature + '_q' + str(q_num)] = pd.qcut(tmp[feature], q=q_num, labels=list(range(1,q_num+1)), duplicates='drop')
+        except Exception as e:
+            tmp[feature + '_q' + str(q_num)] = 3
+        for q in range(1, q_num+1):
+            # 1q 2q 3q 4q 5q
+            df.at[dt, feature + '_q' + str(q)] = tmp.loc[tmp[feature + '_q'+str(q_num)]==q]['returns_2'].sum()
+            
+display_field  = [
+  #'alpha_wt', 'alpha_atr',  'alpha_cci', 'alpha_wtr', 'alpha_rsi', 'alpha_t1','alpha_t5',
+  #'alpha_019', 'alpha_008', 'alpha_078',  'alpha_018', 'alpha_021', 'alpha_022', 
+  #'alpha_110', 'alpha_111', 'alpha_131', 'alpha_112', 'alpha_116', 'alpha_128', 'alpha_133',   
+  'alpha_035', 'alpha_061','alpha_089', 'alpha_033',  'alpha_039', 'alpha_040', 'alpha_083', 
+]
+
+(df[display_field]/5).cumsum().plot()
+
+# alpha_008    7.112507
+# alpha_078    5.924321
+# alpha_019    5.641981
+# alpha_wt     6.093409
+# alpha_cci    5.723536
+# alpha_035    5.557689
+
+feature = 'alpha_035'
+q_df = pd.DataFrame(index = df.index)
+for i in range(1, q_num+1):
+    q_feature = feature + '_q' + str(i)
+    if q_feature in df.columns:
+        q_df[q_feature] = (df[q_feature]).cumsum()
+
+q_df.plot()        
+
+            
+            
+            
+            
+            
+            
+            
+           
