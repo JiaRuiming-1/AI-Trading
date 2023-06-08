216b5f14aa40a31607426e3b942a566563f880a3
JiaRuiming-1 <115370574+JiaRuiming-1@users.noreply.github.com>
2023-06-08 11:53:38 +0800

    Update tmp.py

--- alpha_factor/tmp.py
+++ alpha_factor/tmp.py
@@ -1,8 +1,15 @@
-def rolling_jump(sr, win, interval): #@@@
-    result = []
-    for i in range(win, len(sr), interval):
-        if i + interval <= len(sr):
+def rolling_jump(sr, win, interval):
+    
+    if win>=interval:
+        result = [np.nan]*(win-interval)
+        for i in range(win, len(sr), interval):
             result.extend([np.nan]*(interval-1) + [sr[i-win:i].mean()])
+    else:
+        result = []
+        for i in range(interval, len(sr), interval):
+            result.extend([np.nan]*(interval-1) + [sr[i-win:i].mean()])
+
+    print(i, len(result))
     if len(sr) > len(result):
-        result.extend([np.nan]*(len(sr) % interval))
+        result.extend([np.nan]*(len(sr) - len(result)))
     return pd.Series(result, sr.index)
